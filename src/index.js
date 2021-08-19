b8fa01880b13ebef6161e19f7d4775591c617fba
고동현 <dhjk35@naver.com>
2021-08-19 18:41:15 +0900

    #3. use the map function of JS to Dynamic Component Generation

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// component를 사용하는 방법
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('movie_app')
+    <App />, document.getElementById('movie_app')
 );
